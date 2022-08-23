e61817bc3dd55cd722858cff2a8b1ce35fd6234c
gavlaaaaaaaa <gavlaaaaa5@gmail.com>
2022-08-23 18:13:17 +0100

    added sql injection vulnerability

--- src/main/java/com/contrastsecurity/App.java
+++ src/main/java/com/contrastsecurity/App.java
@@ -1,5 +1,13 @@
 package com.contrastsecurity;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		MysqlDataSource mysqlDS = null;
+		mysqlDS = new MysqlDataSource();
+        mysqlDS.setURL("jdbc:mysql://localhost:3306/test");
+        mysqlDS.setUser("test");
+        mysqlDS.setPassword("test");
+
+        getUser(mysqlDS, "test123");
+    }
+
+    public static void getUser(DataSource dataSource, String userId){
+        try(Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement()){
+            statement.execute("SELECT * FROM user WHERE id = " + userId);
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
